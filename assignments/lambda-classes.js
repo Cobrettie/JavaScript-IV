31bd3eea362f13a1fd8fd1014840ee13da9eb183
Cobrettie <cobrettiegarner3@gmail.com>
2019-04-24 23:04:28 -0400

    Add Students, Instructors

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -10,7 +10,7 @@ class Person {
     }
 
     speak() {
-        return `Hello my name is ${this.name}, I am from ${this.location}.`
+        return `Hello my name is ${this.name}, I am from ${this.location}.`;
     }
 }
 
@@ -24,11 +24,11 @@ class Instructor extends Person {
     }
 
     demo(subject) {
-        return `Today we are learning about ${subject}.`
+        return `Today we are learning about ${subject}.`;
     }
 
     grade(student, subject) {
-        `${student.name} receives a perfect score on ${subject}.`
+        return `${student.name} receives a perfect score on ${subject}.`;
     }
 }
 
@@ -47,10 +47,133 @@ class Student extends Person {
     }
 
     PRAssignment(subject) {
-        return `${this.name} has submitted a PR for ${subject}.`
+        return `${this.name} has submitted a PR for ${subject}.`;
     }
 
     sprintChallenge(subject) {
-        return `${this.name} has begun sprint challenge on ${subject}.`
+        return `${this.name} has begun the sprint challenge on ${subject}.`;
     }
 }
+
+
+class ProjectManager extends Instructor {
+    constructor(pmTraits) {
+        super(pmTraits);
+        this.gradClassName = pmTraits.gradClassName;
+        this.favInstructor = pmTraits.favInstructor;
+    }
+
+    standUp(channel) {
+        return `${this.name} announces to ${channel}, @channel standup times!`;
+    }
+
+    debugsCode() {
+        return `${this.name} debugs ${student.name}'s code on ${subject}`;
+    }
+}
+
+
+// Person
+
+
+const jimmy = new Person({
+    name: 'Jimmy',
+    age: 62,
+    location: 'Florida',
+    gender: 'male'
+});
+
+// console.log(jimmy);
+console.log(jimmy.speak());
+
+
+const kaybear = new Person({
+    name: 'Kay Bear',
+    age: 16,
+    location: 'Florida',
+    gender: 'female'
+});
+
+// console.log(kaybear);
+console.log(kaybear.speak());
+
+
+const happyMan = new Person({
+    name: 'Happy Man',
+    age: 784,
+    location: 'Everywhere',
+    gender: 'male'
+});
+
+// console.log(happyMan);
+console.log(happyMan.speak());
+
+
+// Instructor
+
+
+const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    gender: 'male',
+    favLanguage: 'JavaScript',
+    specialty: 'Front-End',
+    catchPhrase: `Don't forget the homies`
+});
+
+// console.log(fred);
+// console.log(fred.speak());
+console.log(fred.demo('JavaScript IV'));
+console.log(fred.grade(fred, 'JavaScript IV'));
+
+
+const keiran = new Instructor({
+    name: 'Keiran',
+    age: 28,
+    gender: 'male',
+    favLanguage: 'Python',
+    specialty: 'Full-Stack',
+    catchPhrase: 'These are just hot dogs'
+});
+
+// console.log(keiran);
+console.log(keiran.demo('Python!'));
+console.log(keiran.grade(keiran, 'Python'));
+
+
+const dan = new Instructor({
+    name: 'Dan Frehner',
+    age: 32,
+    gender: 'male',
+    favLanguage: 'Everything',
+    specialty: 'UI/UX',
+    catchPhrase: `I'll see you guys tomorrow`
+});
+
+// console.log(dan);
+console.log(dan.demo('UI/UX!!!'));
+console.log(dan.grade(dan, 'UI/UX'));
+
+
+// Student
+
+
+
+const cobrettie = new Student({
+    name: 'Cobrettie',
+    age: 24,
+    location: 'Florida',
+    gender: 'male',
+    favLanguage: 'JavaScript',
+    specialty: 'Full-Stack',
+    previousBackground: 'Park Ranger',
+    className: 'WEBPT6',
+    favSubjects: 'Computer Science, AI, Software Engineering',
+});
+
+
+// console.log(cobrettie);
+// console.log(cobrettie.listsSubjects()); ----- still need to get working
+console.log(cobrettie.PRAssignment('JavaScript-IV'));
+console.log(cobrettie.sprintChallenge('JavaScript-IV'));
